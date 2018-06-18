132d8b8fa44e2f210f1a702fa1a9961110fdfd8c
Tammy Soo <tammy@getfeedback.com>
2018-06-18 14:19:38 -0700

    fixing usage for freestyle and adding new styles

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-addon'
+  name: 'ember-addon',
+
+  included(app, parentAddon) {
+    this._super.included.apply(this, arguments);
+  },
+
+  isDevelopingAddon: function () {
+    return false;
+  }
+
 };
