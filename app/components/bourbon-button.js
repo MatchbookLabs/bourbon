d96da9a444bdd956c29d20f0b5f53797523f236a
Tammy Soo <tammy@getfeedback.com>
2018-06-18 12:01:36 -0700

    creating addon, first commit

---
+++ app/components/bourbon-button.js
@@ -0,0 +1,5 @@
+// shorthand for 
+// import bourbonButton from 'ember-addon/components/bourbon-button';
+// export default bourbonButton;
+
+export { default } from 'ember-addon/components/bourbon-button';
