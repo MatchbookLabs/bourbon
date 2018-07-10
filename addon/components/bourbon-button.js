6ec4fc2ba2106b6f66bb4e6528e0b36de5878640
Tammy Soo <tammy@getfeedback.com>
2018-07-10 11:37:25 -0700

    adding classnamebindings for button types and updating use of quotes

--- addon/components/bourbon-button.js
+++ addon/components/bourbon-button.js
@@ -5,7 +5,23 @@ export default Component.extend({
   layout,
   tagName: 'button',
   classNames: ['bourbon-button'],
+  classNameBindings: ['iconTextButton:bourbon-button--icon-text', 'iconButton:bourbon-button--icon', 'iconOnlyButton:bourbon-button--icon-only'],
   attributeBindings: ['aria-label', 'data-role', 'data-hint',  'target'],
+  iconTextButton: false,
+  iconButton: false,
+  iconOnlyButton: false,
+
+  init() {
+    this._super(...arguments);
+
+    const icon = this.get('icon');
+    const title = this.get('title');
+    const classNames = this.get('class');
+    
+    this.set('iconTextButton', icon && title);
+    this.set('iconOnlyButton', icon && !title && !classNames);
+    this.set('iconButton', icon && !title && classNames);
+  },
 
   click() {
     if (typeof this.get('action') === 'function') {
