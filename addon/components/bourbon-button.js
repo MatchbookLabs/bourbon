63d2914c2c0d326e942e96db6a5c824fa369b29b
Tammy Soo <tammy@getfeedback.com>
2018-07-16 13:05:59 -0700

    clean up styles, add new color for focusing and removing bower and sass

--- addon/components/bourbon-button.js
+++ addon/components/bourbon-button.js
@@ -8,7 +8,7 @@ export default Component.extend({
   tagName: 'button',
   classNames: ['bourbon-button'],
   classNameBindings: ['iconTextButton:bourbon-button--icon-text', 'iconOnlyButton:bourbon-button--icon-only'],
-  attributeBindings: ['aria-label', 'data-role', 'data-hint',  'target'],
+  attributeBindings: ['aria-label', 'data-role', 'data-hint',  'target', 'tabindex'],
   iconTextButton: computed('icon', 'title', function() { return this.get('icon') && this.get('title')}),
   iconOnlyButton: computed('icon', 'title', 'class', function() { return this.get('icon') && !this.get('class')}),
 
