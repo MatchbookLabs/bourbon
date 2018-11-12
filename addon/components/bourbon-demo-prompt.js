908404d32dceb0e53c8382cca88b2c91c07447d6
Tammy Soo <tammy@getfeedback.com>
2018-11-12 09:59:11 -0800

    adding demo prompt

---
+++ addon/components/bourbon-demo-prompt.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+import layout from '../templates/components/bourbon-demo-prompt';
+
+export default Component.extend({
+  tagName: 'div',
+  classNames: ['bourbon-demo-prompt__container'],
+  layout
+});
---
+++ app/components/bourbon-demo-prompt.js
@@ -0,0 +1,1 @@
+export { default } from 'bourbon/components/bourbon-demo-prompt';
