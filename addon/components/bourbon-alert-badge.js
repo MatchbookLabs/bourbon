4e68b575c2a75450e8341459641f0d4bd08e4bce
Tammy Soo <tammy@getfeedback.com>
2018-11-13 16:38:53 -0800

    fix svg causing console error, add alert badge

---
+++ addon/components/bourbon-alert-badge.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from '../templates/components/bourbon-alert-badge';
+
+export default Component.extend({
+  classNames: ['bourbon-alert-badge__container'],
+  layout
+});
