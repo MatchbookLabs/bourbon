674dda579cccf02cfb50e72d398a1169744592b5
Tammy Soo <tammy@getfeedback.com>
2018-11-16 17:53:35 -0800

    making svg a component to be able to nicely resize and change colors

---
+++ addon/components/bourbon-svg--upgrade-badge.js
@@ -0,0 +1,23 @@
+import Component from '@ember/component';
+import layout from '../templates/components/bourbon-svg--upgrade-badge';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  layout,
+  tagName: '',
+  size: 'medium',
+  color: 'black',
+  iconSize: computed('size', function() {
+    if (this.get('size') === 'small') {
+      return '18px';
+    } else if (this.get('size') === 'medium') {
+      return '24px';
+    } else if (this.get('size') === 'large') {
+      return '36px';
+    }
+  }),
+
+  iconColor: computed('color', function() {
+    return `bourbon-text-${this.get("color")} bourbon-fill-current`
+  })
+});
