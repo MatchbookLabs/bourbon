ad877146e1dd737f0b02898ced30f802177c0d1b
Tammy Soo <tammy@getfeedback.com>
2018-12-03 16:35:13 -0800

    demo prompt...suit up!

--- addon/components/bourbon-demo-prompt.js
+++ addon/components/bourbon-demo-prompt.js
@@ -2,13 +2,13 @@ import Component from '@ember/component';
 import layout from '../templates/components/bourbon-demo-prompt';
 
 export default Component.extend({
-  tagName: 'div',
-  classNames: ['bourbon-demo-prompt__container'],
+  tagName: "div",
+  classNames: ["BourbonDemoPrompt-container"],
   layout,
 
   actions: {
     buttonAction() {
-      this.get('buttonAction')();
+      this.get("buttonAction")();
     }
   }
 });
