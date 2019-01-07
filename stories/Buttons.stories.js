7fab55cfb4d4c70037313643361157b5de2278b2
Tammy Soo <tammy@getfeedback.com>
2019-01-07 15:34:57 -0800

    update start script and add a couple additional notes

--- stories/Buttons.stories.js
+++ stories/Buttons.stories.js
@@ -41,17 +41,43 @@ storiesOf("buttons", module)
     }
   )
 
-  .add("secondary button", () => {
-    return {
-      template: hbs`
+  .add(
+    "secondary button",
+    () => {
+      return {
+        template: hbs`
               <div class="BourbonGroupContainer btw-m-4">
                 {{bourbon-button class='BourbonButton--secondary' title='secondary button' action=onClick}}
                 {{bourbon-button title='Disabled' class='BourbonButton--secondary BourbonButton--disabled'}}
               </div>
               {{bourbon-button title='Secondary button' class='BourbonButton--secondary' action=onClick fullWidth=true}}`,
-      context: { onClick: action("buttonClick") }
-    };
-  })
+        context: { onClick: action("buttonClick") }
+      };
+    },
+    {
+      notes: `
+      <h4>Button options</h4>
+      <ul>
+        <li>title (required)</li>
+        <li>fullWidth (optional)</li>
+        <li>action (required)</li>
+        <li>linkTo: to use if you want to open an external link (optional)</li>
+        <li>openLinkInNewTab: default is false. true will open into a new tab (optional)</li>
+      </ul>
+
+      <p>The primary button is used in most situtations to indicate the primary action.</p>
+      <p>All titles should be entered in sentence case.</p>
+      <p>The attributes that can be added to the button currently include the following:</p>
+      <ul>
+        <li>aria-label</li>
+        <li>tabindex</li>
+        <li>data-role</li>
+        <li>target</li>
+        <li>data-hint</li>
+      </ul>
+      `
+    }
+  )
 
   .add("rounded button", () => {
     return {
@@ -73,8 +99,33 @@ storiesOf("buttons", module)
     };
   })
 
-  .add("icon button", () => {
-    return {
-      template: hbs`{{bourbon-button icon='edit-button'}}`
-    };
-  });
+  .add(
+    "icon button",
+    () => {
+      return {
+        template: hbs`{{bourbon-button icon='edit-button'}}`
+      };
+    },
+    {
+      notes: `
+      <h4>Button options</h4>
+      <ul>
+        <li>title (required)</li>
+        <li>fullWidth (optional)</li>
+        <li>action (required)</li>
+        <li>linkTo: to use if you want to open an external link (optional)</li>
+        <li>openLinkInNewTab: default is false. true will open into a new tab (optional)</li>
+      </ul>
+
+      <p>The primary button is used in most situtations to indicate the primary action.</p>
+      <p>All titles should be entered in sentence case.</p>
+      <p>The attributes that can be added to the button currently include the following:</p>
+      <ul>
+        <li>aria-label</li>
+        <li>tabindex</li>
+        <li>data-role</li>
+        <li>target</li>
+        <li>data-hint</li>
+      </ul>
+      `
+    });
