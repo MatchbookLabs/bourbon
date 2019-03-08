f08d680966d62710eabbe45bfe30dde63738220f
Tammy Soo <tammy@getfeedback.com>
2019-03-08 12:08:07 -0800

    5651: fixing spec

--- tests/integration/components/bourbon-toggle-test.js
+++ tests/integration/components/bourbon-toggle-test.js
@@ -6,21 +6,13 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | bourbon-toggle', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders on toggle', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{bourbon-toggle value=true}}`);
-
-    assert.equal(this.element.textContent.replace(/\s+/g, '').trim(), 'ON');
-  });
-
-  test('it renders off toggle', async function (assert) {
+  test('it renders toggle', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`{{bourbon-toggle value=false}}`);
 
-    assert.equal(this.element.textContent.replace(/\s+/g, '').trim(), 'OFF');
+    assert.equal(this.element.firstChild.classList[0], 'BourbonToggle');
+    assert.equal(this.element.firstChild.tagName, 'LABEL');
   });
 });
