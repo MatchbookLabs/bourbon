429d6113c5b9e7b1f8d8a87c1f4c8380b5e6fa2c
Tammy Soo <tammy@getfeedback.com>
2019-06-25 14:00:00 -0700

    6431: adding new interim value to prevent safari cursor jumping issue

--- addon/components/bourbon-text-field.js
+++ addon/components/bourbon-text-field.js
@@ -19,6 +19,7 @@ export default Component.extend({
   autofocus: false,
   readonly: null,
   value: null,
+  textInput: null,
   isFocused: false,
   noLabel: false,
 
@@ -57,11 +58,15 @@ export default Component.extend({
   input(e) {
     let el = $(e.currentTarget);
     let textInput = el.find('.BourbonTextField-input').val();
-    this.set('value', textInput);
+
+    // setting value directly here was causing the cursor to
+    // jump to the end of the input field in safari.
+    this.set('textInput', textInput);
   },
 
   focusOut() {
     this.set('isFocused', false);
+    this.set('value', this.get('textInput'));
     document.activeElement.blur();
 
     if (this.get('onFocusOutOrEnter')) {
@@ -75,6 +80,8 @@ export default Component.extend({
 
   keyDown(e) {
     if (e.keyCode === 13) {
+      this.set('value', this.get('textInput'));
+
       if (this.get('actionOnEnter')) {
         this.get('actionOnEnter')(this.get('value'));
       }
