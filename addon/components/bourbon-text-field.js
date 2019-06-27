e4a61c3f1c50d1e79d01b88c67820389cb84812d
Tammy Soo <tammy@getfeedback.com>
2019-06-27 16:08:47 -0700

    6431: observer, not computed

--- addon/components/bourbon-text-field.js
+++ addon/components/bourbon-text-field.js
@@ -37,8 +37,8 @@ export default Component.extend({
     this.set('isFocused', this.get('autofocus'));
   }),
 
-  isNotEmpty: computed('textInput', function() {
-    return this.get('textInput') && !this.get('noLabel');
+  isNotEmpty: computed('value', function() {
+    return this.get('value') && !this.get('noLabel');
   }),
 
   didInsertElement() {
@@ -65,7 +65,7 @@ export default Component.extend({
     this.set('textInput', textInput);
   },
 
-  valueObserver: computed('textInput', function() {
+  valueObserver: observer('textInput', function() {
     this.set('value', this.get('textInput'));
   }),
 
