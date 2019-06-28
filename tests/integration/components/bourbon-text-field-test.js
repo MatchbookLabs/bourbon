76a3a04570fdfa4d57150a13b6ae19afa12f8078
Tammy Soo <tammy@getfeedback.com>
2019-06-28 10:24:13 -0700

    6341: clean up test

--- tests/integration/components/bourbon-text-field-test.js
+++ tests/integration/components/bourbon-text-field-test.js
@@ -16,19 +16,12 @@ module('Integration | Component | bourbon-text-field', function(hooks) {
   });
 
   test('it is disabled', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{bourbon-text-field disabled=true}}`);
 
     assert.equal(this.$('.BourbonTextField-input').attr('disabled'), 'disabled');
   });
 
   test('it has a placeholder', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-
     await render(hbs`{{bourbon-text-field placeholder="hello world"}}`);
 
     assert.equal(this.element.children[0].textContent.trim(), 'hello world');
